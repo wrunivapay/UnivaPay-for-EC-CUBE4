a1336157d3347e67bec2ba16d8318164aaf777e3
5ym <16yuim@gmail.com>
2021-11-08 13:28:50 +0900

    fix subscritption

--- Controller/SubscriptionController.php
+++ Controller/SubscriptionController.php
@@ -13,6 +13,7 @@ use Eccube\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Plugin\UnivaPay\Util\SDK;
 use Plugin\UnivaPay\Repository\ConfigRepository;
