ba6903aa8095a7bf91a1970f51be65d091cf7562
5ym <16yuim@gmail.com>
2021-11-14 14:46:56 +0900

    add charge id search

---
+++ Repository/ChargeIdSearchCustomizer.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Plugin\UnivaPay\Repository;
+
+    use Eccube\Doctrine\Query\QueryCustomizer;
+    use Eccube\Repository\QueryKey;
+    use Eccube\Util\StringUtil;
+    use Doctrine\ORM\QueryBuilder;
+
+    /**
+    *
+    */
+    class ChargeIdSearchCustomizer implements QueryCustomizer {
+
+        /**
+        *  課金IDが検索されるようにクエリを追加
+        */
+        public function customize(QueryBuilder $qb, $searchData, $queryKey)
+        {
+            if (isset($searchData['multi']) && StringUtil::isNotBlank($searchData['multi'])) {
+                // 注文番号検索の場合を除外
+                if(!preg_match('/^\d{0,10}$/', $searchData['multi'])) $qb->orWhere('o.univa_pay_charge_id like :likemulti');
+            }
+        }
+
+        public function getQueryKey(): string
+        {
+            return QueryKey::ORDER_SEARCH_ADMIN;
+        }
+
+    }
