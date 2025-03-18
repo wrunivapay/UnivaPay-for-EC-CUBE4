326a098a5a59954a7ff37adbc7af42101d1b0e28
Roy Wijaya <r.wijaya@univapay.com>
2025-03-18 16:38:04 +0900

    cleanup

--- EventListener/CartValidationEventListener.php
+++ EventListener/CartValidationEventListener.php
@@ -6,38 +6,27 @@ use Eccube\Event\EccubeEvents;
 use Eccube\Event\EventArgs;
 use Eccube\Service\CartService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Plugin\UnivaPay\Entity\SubscriptionPeriod;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Core\Security;
 
 class CartValidationEventListener implements EventSubscriberInterface
 {
     private $cartService;
     private $session;
-    private $router;
-    private $security;
 
     public function __construct(
         SessionInterface $session,
-        CartService $cartService,
-        RouterInterface $router,
-        Security $security
+        CartService $cartService
     ) {
         $this->cartService = $cartService;
         $this->session = $session;
-        $this->router = $router;
-        $this->security = $security;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            EccubeEvents::FRONT_PRODUCT_CART_ADD_COMPLETE => 'onCartComplete',
-            EccubeEvents::FRONT_SHOPPING_LOGIN_INITIALIZE => 'onShopping'
+            EccubeEvents::FRONT_PRODUCT_CART_ADD_COMPLETE => 'onCartComplete'
         ];
     }
 
@@ -58,25 +47,5 @@ class CartValidationEventListener implements EventSubscriberInterface
             $this->session->getFlashBag()->set('eccube.front.request.error', trans('univa_pay.error.cart.subscription.only_one_item'));
             throw new BadRequestHttpException(trans('univa_pay.error.cart.subscription.only_one_item'));
         }
-    }
-
-    // only allow member to purchase subscription item
-    public function onShopping(EventArgs $event)
-    {
-        // $hasSubsciption = false;
-
-        // foreach ($this->cartService->getCart()->getCartItems() as $item) {
-        //     $subscriptionPeriod = $item->getProductClass()->getSubscriptionPeriod();
-        //     if ($subscriptionPeriod !== null && $subscriptionPeriod !== SubscriptionPeriod::NON_SUBSCRIPTION) {
-        //         $hasSubsciption = true;
-        //         break;
-        //     }
-        // }
-
-        // if ($hasSubsciption && !$this->security->getUser()) {
-        //     $this->session->getFlashBag()->add('eccube.front.request.error', trans('univa_pay.error.login_required_for_subscription'));
-        //     $response = new RedirectResponse($this->router->generate('mypage_login'));
-        //     $event->setResponse($response);
-        // }
     }
 }
