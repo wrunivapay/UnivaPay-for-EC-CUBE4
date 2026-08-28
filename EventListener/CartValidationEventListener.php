f07a2e5d7770b41dee33dfccea5d66f265ec0302
Roy <r.wijaya@univapay.com>
2026-08-28 14:25:21 +0900

    Fix Cart Validation (#17)
    * fix cart validation
    
    * add test

--- EventListener/CartValidationEventListener.php
+++ EventListener/CartValidationEventListener.php
@@ -5,47 +5,91 @@ namespace Plugin\UnivaPay\EventListener;
 use Eccube\Event\EccubeEvents;
 use Eccube\Event\EventArgs;
 use Eccube\Service\CartService;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Eccube\Service\PurchaseFlow\PurchaseContext;
+use Eccube\Service\PurchaseFlow\PurchaseFlow;
 use Plugin\UnivaPay\Entity\SubscriptionPeriod;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class CartValidationEventListener implements EventSubscriberInterface
 {
     private $cartService;
     private $session;
+    private $router;
+    private $cartPurchaseFlow;
+    private $tokenStorage;
 
     public function __construct(
         CartService $cartService,
-        SessionInterface $session
+        SessionInterface $session,
+        RouterInterface $router,
+        PurchaseFlow $cartPurchaseFlow,
+        TokenStorageInterface $tokenStorage
     ) {
         $this->cartService = $cartService;
         $this->session = $session;
+        $this->router = $router;
+        $this->cartPurchaseFlow = $cartPurchaseFlow;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            EccubeEvents::FRONT_PRODUCT_CART_ADD_COMPLETE => 'onCartComplete'
+            EccubeEvents::FRONT_PRODUCT_CART_ADD_COMPLETE => 'onCartComplete',
         ];
     }
 
     // only allow one subscription item in the cart
     public function onCartComplete(EventArgs $event)
     {
-        $hasSubsciption = false;
+        $hasSubscription = false;
 
         foreach ($this->cartService->getCart()->getCartItems() as $item) {
             $subscriptionPeriod = $item->getProductClass()->getSubscriptionPeriod();
-            if ($subscriptionPeriod !== null && $subscriptionPeriod !== SubscriptionPeriod::NON_SUBSCRIPTION) {
-                $hasSubsciption = true;
+            if ($subscriptionPeriod !== null && $subscriptionPeriod->getId() !== SubscriptionPeriod::NON_SUBSCRIPTION) {
+                $hasSubscription = true;
                 break;
             }
         }
 
-        if (count($this->cartService->getCart()->getCartItems()) > 1 && $hasSubsciption) {
-            $this->session->getFlashBag()->set('eccube.front.request.error', trans('univa_pay.error.cart.subscription.only_one_item'));
-            throw new BadRequestHttpException(trans('univa_pay.error.cart.subscription.only_one_item'));
+        if ($hasSubscription && count($this->cartService->getCart()->getCartItems()) > 1) {
+            // the item was already persisted by CartService::save() before this event
+            $addCartData = $event->getArgument('form')->getData();
+            $this->cartService->removeProduct($addCartData['product_class_id']);
+            // recalculate
+            foreach ($this->cartService->getCarts() as $Cart) {
+                $this->cartPurchaseFlow->validate($Cart, new PurchaseContext($Cart, $this->getCurrentUser()));
+            }
+            $this->cartService->save();
+
+            $message = trans('univa_pay.error.cart.subscription.only_one_item');
+            $request = $event->getRequest();
+
+            if ($request->isXmlHttpRequest()) {
+                $this->session->getFlashBag()->add('eccube.front.request.error', $message);
+            } else {
+                $this->session->getFlashBag()->add('eccube.front.request.error', $message);
+                $event->setResponse(new RedirectResponse($this->router->generate('cart')));
+            }
         }
+    }
+
+    private function getCurrentUser()
+    {
+        $token = $this->tokenStorage->getToken();
+        if (!$token) {
+            return null;
+        }
+
+        if (!is_object($user = $token->getUser())) {
+            return null;
+        }
+
+        return $user;
     }
 }
